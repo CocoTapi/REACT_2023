fa0e117566e5f3a85fc0156d4c9eca55ac1fd462
Chocotapi <shioriavino@gmail.com>
2023-06-16 12:57:11 -0400

    changed div to Card (No content showed)

--- section_3/src/components/ExpenseItem.js
+++ section_3/src/components/ExpenseItem.js
@@ -1,6 +1,8 @@
 
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
+
 
 function ExpenseItem(props) {
     const {title, amount, date} = props.item;
@@ -14,13 +16,14 @@ function ExpenseItem(props) {
     // const year = props.date.getFullYear();
 
     return (
-    <div className="expense-item"> 
+    //changed <div> to <Card>
+    <Card className="expense-item"> 
         <ExpenseDate date={date}></ExpenseDate>
         <div className="expense-item__description">
             <h2>{title}</h2>
             <div className="expense-item__price">{amount}</div>
         </div> 
-    </div>
+    </Card>
     )
 }
 
