097a0423ca123063bfcd302a1d3b48948fc6d664
Chocotapi <shioriavino@gmail.com>
2023-06-15 23:29:21 -0400

    made a dinamic component

--- section_3/src/components/ExpenseItem.js
+++ section_3/src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const {title, amount, date} = props.item;
+
+// const expenseDate = new Date(2023, 6, 15);
+// const expenseTitle = "Dodger's food";
+// const expenseAmount = 37.99;
+
     return (
     <div className="expense-item">
-            <div>Dodger's Food</div>
+            <div>{date.toDateString()}</div>
         <div className="expense-item__description">
-            <h2 className="expense-item">Purina One</h2>
-            <div className="expense-item__price">30.99</div>
+            <h2 className="expense-item">{title}</h2>
+            <div className="expense-item__price">{amount}</div>
         </div> 
        
     </div>
