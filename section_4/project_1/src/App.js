7bbbb2bc4d75ee76c72b0b2e62bdf0fcc6ca195d
Chocotapi <shioriavino@gmail.com>
2023-06-16 23:01:52 -0400

    added 3 comcepts

--- section_4/project_1/src/App.js
+++ section_4/project_1/src/App.js
@@ -36,7 +36,17 @@ function App() {
         <li className="concept">
           <img src={concepts[0].image} alt={concepts[0].title} />
           <h2></h2>
-          <p>TODO: DESCRIPTION</p>
+          <p>{concepts[0].description}</p>
+        </li>
+        <li className="concept">
+          <img src={concepts[1].image} alt={concepts[1].title} />
+          <h2></h2>
+          <p>{concepts[1].description}</p>
+        </li>
+        <li className="concept">
+          <img src={concepts[2].image} alt={concepts[2].title} />
+          <h2></h2>
+          <p>{concepts[2].description}</p>
         </li>
       </ul>
     </div>
