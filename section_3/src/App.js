3dad469ee5930eb754b57041f47fcff0007da569
Chocotapi <shioriavino@gmail.com>
2023-06-17 17:42:13 -0400

    added code to communicate from child-parent

--- section_3/src/App.js
+++ section_3/src/App.js
@@ -2,9 +2,14 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App () {
+  const addExpenseHandler = expense => {
+    console.log("In App.js");
+    console.log(expense);
+  }
+
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <h2>Our Expense for a Month</h2>
       <Expenses></Expenses>
     </div>
