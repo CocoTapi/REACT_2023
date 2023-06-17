0b05c2209467649b79f4ad61fc1365b5b6a52048
Chocotapi <shioriavino@gmail.com>
2023-06-17 14:14:22 -0400

    made new components for new expenses

---
+++ section_3/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return (
+    <div className='new-expense'>
+        <from></from>
+    </div>
+    )
+}
+
+export default NewExpense;
