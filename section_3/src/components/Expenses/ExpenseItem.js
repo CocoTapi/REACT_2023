57fc525b3870c4abd613eb08186126ae6bc86e5d
Chocotapi <shioriavino@gmail.com>
2023-06-17 13:55:36 -0400

    added comments about updating

--- section_3/src/components/Expenses/ExpenseItem.js
+++ section_3/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,16 +7,26 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
     const {title, amount, date} = props.item;
+    const [newTitle, setTitle] =  useState(title);
+    //must be inside a function. This lead the function calls again 
+    //useState(This is something that is shown before clicking and something that you want to update)
+   
+    const clickHandler = () => {
+        //shows "Updated" on the screen 
+        setTitle('Updated');
+        //setTitle(This is what you want to show after clicking)
+        console.log(newTitle);
+    };
 
     return (
     //changed <div> to <Card>
     <Card className="expense-item"> 
         <ExpenseDate date={date}></ExpenseDate>
         <div className="expense-item__description">
-            <h2>{title}</h2>
+            <h2>{newTitle}</h2>
             <div className="expense-item__price">${amount}</div>
         </div> 
-        <button>Change Title</button>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>
     )
 }
