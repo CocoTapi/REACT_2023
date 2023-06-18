5b937ada06f0ea071c5edc352070e7ce720a4799
Chocotapi <shioriavino@gmail.com>
2023-06-18 12:44:30 -0400

    connected a child to parent and set a default year

--- section_3/src/components/Expenses/Expenses.js
+++ section_3/src/components/Expenses/Expenses.js
@@ -1,6 +1,9 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import "./Expenses.css";
+import ExpenseFilter from "./ExpenseFilter";
 
 const expenses = [
     {
@@ -27,9 +30,18 @@ const expenses = [
 
 
 function Expenses(props) {
+  //for updating states  
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        console.log('Expenses.js');
+        setFilteredYear(selectedYear);
+    }
 
     return (
       <Card className="expenses">
+        {/* selected={filteredYear} is for default. In ExpenseFilter,js, add value in <select>*/}
+        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpenseFilter>
         <ExpenseItem
           item={expenses[0]}
         ></ExpenseItem>
