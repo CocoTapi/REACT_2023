b80a69cec03c410c2c3424bf772aa89674b27c9b
Chocotapi <shioriavino@gmail.com>
2023-06-20 16:31:50 -0400

    set a button

--- section_6/section_3/src/components/NewExpense/NewExpense.js
+++ section_6/section_3/src/components/NewExpense/NewExpense.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
-const saveExpenseDataHandler = (enteredExpenseData) => {
-    const expenseData = {
-        ...enteredExpenseData,
-        id: Math.random().toString()
-    };
+    const [isEditing, setIsEditing] = useState(false);
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
     // console.log(expenseData);
 
     // this communicate with App.js
     props.onAddExpense(expenseData);
-}
+    }
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
 
     return (
     <div className='new-expense'>
+        <button onClick={startEditingHandler}>Add New Expense</button>
         <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
         {/* you don't have to name onSaveExoenseData. Any name is fine */}
     </div>
