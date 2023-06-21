65b7bedaf089a8dbcd98d3d8d70f79f4283bb8d8
Chocotapi <shioriavino@gmail.com>
2023-06-21 17:37:19 -0400

    added styles in JS with package "styled-component"

--- section_7/src/components/CourseGoals/CourseInput/CourseInput.js
+++ section_7/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,44 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+  }
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background: #ffd7d7;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -28,8 +65,8 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''} `}>
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+      <FormControl className={!isValid && 'invalid'}>
+        <label>Course Goal</label>
         <input 
           style={{
             borderColor: !isValid ? 'red' : '#ccc', 
@@ -37,10 +74,13 @@ const CourseInput = props => {
           }} 
           type="text" 
           onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
 
 export default CourseInput;
+
+//line 69: if there is no <FormControl> --> add this to the label: style={{color: !isValid ? 'red' : 'black'}}
+//Or <FormControl className={!isValid && 'invalid'}> Nothing in <label>
