c9c9187861eb018b1904846418f532637ff41c6e
Chocotapi <shioriavino@gmail.com>
2023-06-20 23:54:04 -0400

    finished setting up a chart

--- section_6/section_3/src/components/Chart/Chart.js
+++ section_6/section_3/src/components/Chart/Chart.js
@@ -4,11 +4,19 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = props => {
+    //calicurate the maximum amount
+    //Array of dataPointValues changes from objects to numbers
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+
+    //Math.max wants a list of arguments, not array. 
+    //so '...' pulls out all the array elements and adds them as standalone arguments
+    const totalMaximum = Math.max(...dataPointValues);
+
     return <div className="chart">
         {props.dataPoints.map(dataPoint => (
             <ChartBar 
             value={dataPoint.value} 
-            maxValue={null} 
+            maxValue={totalMaximum} 
             label={dataPoint.label}/>
             ))}
     </div>
