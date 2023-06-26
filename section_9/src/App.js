21f79f00728334322556cdaac61dc301ec6df6d7
Chocotapi <shioriavino@gmail.com>
2023-06-25 22:33:55 -0400

    made a new component for table

--- section_9/src/App.js
+++ section_9/src/App.js
@@ -1,5 +1,6 @@
 import logo from './assets/investment-calculator-logo.png';
 import SavingsForm from './components/SavingsForm';
+import ResultTable from '../../section_8/src/components/ResultTable';
 
 function App() {
   const calculateHandler = (userInput) => {
@@ -41,26 +42,7 @@ function App() {
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <ResultTable></ResultTable>
     </div>
   );
 }
