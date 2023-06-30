66134bf2240a5710443481cb9610f2841bf01ddf
Chocotapi <shioriavino@gmail.com>
2023-06-30 00:44:04 -0400

    added nice format for numbers

--- section_9/formProjectPt1/src/components/ResultTable.js
+++ section_9/formProjectPt1/src/components/ResultTable.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+//formatting for numbers
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+});
+
 const ResultTable = (props) => {
     return (
         <table className="result">
@@ -16,16 +23,16 @@ const ResultTable = (props) => {
           {props.data.map((yearData) => {
             <tr key={yearData.year}>
               <td> {yearData.year} </td>
-              <td> {yearData.savingsEndOfYear} </td>
-              <td> {yearData.yearlyInterest} </td>
+              <td> {formatter.format(yearData.savingsEndOfYear)} </td>
+              <td> {formatter.format(yearData.yearlyInterest)} </td>
               <td> 
-                {yearData.savingsEndOfYear - 
+                {formatter.format(yearData.savingsEndOfYear - 
                   props.initialInvestment - 
-                  yearData.yearlyContribution * yearData.year} 
+                  yearData.yearlyContribution * yearData.year)} 
               </td>
               <td> 
-                {props.initialInvestment + 
-                  yearData.yearlyContribution * yearData.year} 
+                {formatter.format(props.initialInvestment + 
+                  yearData.yearlyContribution * yearData.year)} 
               </td>
             </tr>
           })}
