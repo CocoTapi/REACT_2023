e3d0af7aeee63ba644f6067ba4dde521f98c1308
Chocotapi <shioriavino@gmail.com>
2023-06-29 23:59:59 -0400

    fixed name to show numbers as a default

--- section_9/formProjectPt1/src/components/SavingsForm.js
+++ section_9/formProjectPt1/src/components/SavingsForm.js
@@ -8,7 +8,7 @@ const initialUserInput = {
 }
 
 const SavingsForm = () => {
-    const [useInput, setUserInput] = useState({
+    const [userInput, setUserInput] = useState({
         'current-savings': 10000,
         'yearly-contribution': 1200,
         'expected-return': 7,
@@ -68,11 +68,11 @@ const SavingsForm = () => {
             <div className="input-group">
                 <p>
                     <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input onChange={(event) => inputChangeHandler('current-savings', event.target.value)} value={SavingsForm['current-savings']} type="number" id="current-savings" />
+                    <input onChange={(event) => inputChangeHandler('current-savings', event.target.value)} value={userInput['current-savings']} type="number" id="current-savings" />
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-                    <input onChange={(event) => inputChangeHandler("yearly-contribution", event.target.value)} value={SavingsForm['yearly-contribution']} type="number" id="yearly-contribution" />
+                    <input onChange={(event) => inputChangeHandler('yearly-contribution', event.target.value)} value={userInput['yearly-contribution']} type="number" id="yearly-contribution" />
                 </p>
 
             </div>
@@ -82,11 +82,11 @@ const SavingsForm = () => {
                     <label htmlFor="expected-return">
                         Expected Interest (%, per year)
                     </label>
-                    <input onChange={(event) => inputChangeHandler("expected-return", event.target.value)} value={SavingsForm['expected-return']} type="number" id="expected-return" />
+                    <input onChange={(event) => inputChangeHandler('expected-return', event.target.value)} value={userInput['expected-return']} type="number" id="expected-return" />
                 </p>
                 <p>
                     <label htmlFor="duration">Investment Duration (years)</label>
-                    <input onChange={(event) => inputChangeHandler("duration", event.target.value)} value={SavingsForm['duration']} type="number" id="duration" />
+                    <input onChange={(event) => inputChangeHandler('duration', event.target.value)} value={userInput['duration']} type="number" id="duration" />
                 </p>
             </div>
                 <p className="actions">
