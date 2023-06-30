9c161b856838361d7b205ef14f727f94a145ae35
Chocotapi <shioriavino@gmail.com>
2023-06-30 00:04:14 -0400

    set a function to lift up the data

--- section_9/formProjectPt1/src/components/SavingsForm.js
+++ section_9/formProjectPt1/src/components/SavingsForm.js
@@ -7,7 +7,7 @@ const initialUserInput = {
     'duration': 10
 }
 
-const SavingsForm = () => {
+const SavingsForm = (props) => {
     const [userInput, setUserInput] = useState({
         'current-savings': 10000,
         'yearly-contribution': 1200,
@@ -17,7 +17,8 @@ const SavingsForm = () => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        console.log('SUBMIT');
+        
+        props.onCalculate(userInput);
     };
 
     const resetHandler = () => {
@@ -31,36 +32,7 @@ const SavingsForm = () => {
                 [input]: value,
             }
         });
-    };
-
-    const calculateHandler = (userInput) => {
-        // Should be triggered when form is submitted
-        // You might not directly want to bind it to the submit event on the form though...
-    
-        const yearlyData = []; // per-year results
-    
-        let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-        const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-        const expectedReturn = +userInput['expected-return'] / 100;
-        const duration = +userInput['duration'];
-    
-        // The below code calculates yearly results (total savings, interest etc)
-        for (let i = 0; i < duration; i++) {
-          const yearlyInterest = currentSavings * expectedReturn;
-          currentSavings += yearlyInterest + yearlyContribution;
-          
-          yearlyData.push({
-            // feel free to change the shape of the data pushed to the array!
-            year: i + 1,
-            yearlyInterest: yearlyInterest,
-            savingsEndOfYear: currentSavings,
-            yearlyContribution: yearlyContribution,
-          });
-        }
-    
-        // do something with yearlyData ...
-      };
-    
+    };    
 
 
     return (
