fca4377a4ae2bbc6484978e7b2c452b82c60cf5c
Chocotapi <shioriavino@gmail.com>
2023-07-06 23:42:24 -0400

    added min and max age

--- section_9/userDataProject/src/components/Users/AddUser.js
+++ section_9/userDataProject/src/components/Users/AddUser.js
@@ -35,7 +35,7 @@ const AddUser = (props) => {
                 <label htmlFor='username' >Username</label>
                 <input id='username' type='text' onChange={(event) => inputChangeHandler('username', event.target.value)} value={enteredUser['username']}></input>
                 <label htmlFor='age'>Age</label>
-                <input id='age' type='number' onChange={(event) => inputChangeHandler('age', event.target.value)} value={enteredUser['age']}></input>
+                <input id='age' type='number' min='1' max='99' onChange={(event) => inputChangeHandler('age', event.target.value)} value={enteredUser['age']}></input>
                 <Button type='submit' onClick={addUserHandler}>Add User</Button>
             </form>
         </Card>
