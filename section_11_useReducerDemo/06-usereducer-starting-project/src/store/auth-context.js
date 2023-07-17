85f62702545e73414d208ec63b92d5f651a1efd7
Chocotapi <shioriavino@gmail.com>
2023-07-16 22:10:13 -0400

    put one central context with one state management

--- section_11_useReducerDemo/06-usereducer-starting-project/src/store/auth-context.js
+++ section_11_useReducerDemo/06-usereducer-starting-project/src/store/auth-context.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 
 const AuthContext = React.createContext({
-    isLoggledIn: false
+    isLoggledIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    
+        if (storedUserLoggedInInformation === '1') {
+          setIsLoggedIn(true);
+        }
+      }, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    }
+
+    const loginHandler = () => {
+        setIsLoggedIn(true);
+    };
+
+    return ( 
+        <AuthContext.Provider 
+            value={{
+                isLoggedIn: isLoggedIn,
+                onLogout: logoutHandler, 
+                onLogin: loginHandler,
+            }}
+        >
+        {PaymentResponse.children}
+        </AuthContext.Provider>
+    )
+}
 
 export default AuthContext;
