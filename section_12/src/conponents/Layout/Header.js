f9ce6337b995aa7f92a6438ee2eddff2387e53d7
Chocotapi <shioriavino@gmail.com>
2023-07-17 15:47:28 -0400

    added a header cart button

--- section_12/src/conponents/Layout/Header.js
+++ section_12/src/conponents/Layout/Header.js
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 
 import classes from './Header.module.css';
 import entranceImage from '../../assets/entrance.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return (
         <Fragment>
-            <heafer className={classes.heafer}>
+            <heafer className={classes.header}>
                 <h1>Katsu-tei Hirosaki</h1>
-                <button>Cart</button>
+                <HeaderCartButton></HeaderCartButton>
             </heafer>
             <div className={classes['main-image']}>
                 <img src={entranceImage} alt='Katsu-tei entrance'></img>
