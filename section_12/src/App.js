824f95f10e283c8a3339b08c222a3c1990ed8fe9
Chocotapi <shioriavino@gmail.com>
2023-07-18 00:09:10 -0400

    added functions to show/close cart

--- section_12/src/App.js
+++ section_12/src/App.js
@@ -1,14 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Header from "./conponents/Layout/Header";
 import Meals from './conponents/Meals/Meals';
 import Cart from './conponents/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCardHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
-      <Cart></Cart>
-      <Header></Header>
+      {cartIsShown && <Cart />}
+      <Header />
       <main>
         <Meals />
       </main>
