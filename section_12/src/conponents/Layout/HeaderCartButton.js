3c56dc7b411f988b84ebd2c4e8256a733154f5e0
Chocotapi <shioriavino@gmail.com>
2023-07-20 15:19:32 -0400

    added the first time bump animation

--- section_12/src/conponents/Layout/HeaderCartButton.js
+++ section_12/src/conponents/Layout/HeaderCartButton.js
@@ -1,19 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../store/cart-context";
 
 const HeaderCartButton = (props) => {
+    //for animation
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     //cart button content re-evaluated by react whenever the context changes and
     //when we updated it in the cart provider
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems =  cartCtx.items.reduce((currentNumber, item) => {
+    const { items } = cartCtx;
+
+    const numberOfCartItems =  items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0)
 
-    const btnClasses = `${classes.button} ${classes.bump}`;
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+    }, [items]);
 
     return (
         <button className={btnClasses} onClick={props.onClick}>
