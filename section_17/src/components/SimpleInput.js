8f2eba9556d55d244e9232d5b847119d977a46bd
Chocotapi <shioriavino@gmail.com>
2023-12-01 15:49:51 -0500

    clean the code

--- section_17/src/components/SimpleInput.js
+++ section_17/src/components/SimpleInput.js
@@ -1,31 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const SimpleInput = (props) => {
   //const nameInputRef = useRef('');
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if (enteredNameIsValid){
-      console.log("Name Input is valid")
-    }
-  }, [enteredNameIsValid]);
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-
-    if (event.target.value.trim() !== '') {
-      setEnteredNameIsValid(true);
-    }
   }; 
 
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true)
-
-    if (event.target.value.trim() === '') {
-      setEnteredNameIsValid(false);
-    }
   };
 
   const formSubmissionHandler = (event) => {
@@ -33,12 +23,9 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-
-    setEnteredNameIsValid(true);
 
     console.log(enteredName);
 
@@ -47,9 +34,8 @@ const SimpleInput = (props) => {
 
     //nameInputRef.current.value = ''; => Not IdleDeadline. Don't manipulate the dom!
     setEnteredName('');
+    setEnteredNameTouched(false);
   }
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
 
   const nameInputClasses = nameInputIsInvalid 
     ? 'form-control invalid' 
