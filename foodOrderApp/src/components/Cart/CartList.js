40d9f7297b00f89eca8e955110dcad2df749cfab
Chocotapi <shioriavino@gmail.com>
2023-12-04 14:07:36 -0500

    added styling

--- foodOrderApp/src/components/Cart/CartList.js
+++ foodOrderApp/src/components/Cart/CartList.js
@@ -1,16 +1,22 @@
 import Button from "../UI/Button";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
-
+import classes from './CartList.module.css';
 function CartList (props) {
     return (
         <Modal>
-            <CartItem />
-            <h2>Total Amount</h2>
-            <div>$100</div>
-            <div>
-                <Button onClick={props.onClick}>Close</Button>
-                <Button>Order</Button>
+            <div className={classes.cart}>
+                <CartItem />
+                <h2 className={classes.total}>Total Amount</h2>
+                <div>$100</div>
+                <div>
+                    <Button 
+                        onClick={props.onClick}
+                    >
+                        Close
+                    </Button>
+                    <Button>Order</Button>
+                </div>
             </div>
         </Modal>
     )
