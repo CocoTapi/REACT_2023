e460e3b0fbb076b9a4994de5610a22d375a3b4f1
Chocotapi <shioriavino@gmail.com>
2023-12-03 19:42:24 -0500

    moved login/out functions with context provider

--- useReducerPractice/src/components/Login/Login.js
+++ useReducerPractice/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const initialEmailState = {
   value: '',
@@ -43,6 +44,8 @@ const Login = (props) => {
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, initialEmailState);
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, initialPasswordState) 
+
+  const authCtx = useContext(AuthContext);
 
   // useEffect(() => {
   //   console.log('EFFECT RUNNING');
@@ -95,7 +98,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
