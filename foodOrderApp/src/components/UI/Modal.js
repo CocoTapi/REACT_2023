8d013121ab6e9f303060c9f0bbcfb0d5a4957cd0
Chocotapi <shioriavino@gmail.com>
2023-12-05 23:13:00 -0500

    created portal for the modal

--- foodOrderApp/src/components/UI/Modal.js
+++ foodOrderApp/src/components/UI/Modal.js
@@ -1,13 +1,29 @@
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
 import classes from './Modal.module.css';
 
-function Modal (props) {
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} />
+};
+
+const ModalOverlay = (props) => {
     return (
-        <div className={classes.backdrop} >
-            <div className={classes.modal}>
+        <div className={classes.modal} >
+            <div className={classes.content}>
                 {props.children}
             </div>
         </div>
     )
 };
+
+const Modal = () => {
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(<Backdrop />)}
+            {ReactDOM.createPortal(<ModalOverlay />)}
+        </Fragment>
+    )
+}
 
 export default Modal;
