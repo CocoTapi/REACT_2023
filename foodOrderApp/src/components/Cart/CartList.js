62b103e8ac1ca4fa909bde771ed6ee43af4227b9
Chocotapi <shioriavino@gmail.com>
2023-12-05 23:04:18 -0500

    updated CartList component

--- foodOrderApp/src/components/Cart/CartList.js
+++ foodOrderApp/src/components/Cart/CartList.js
@@ -2,21 +2,24 @@ import Button from "../UI/Button";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import classes from './CartList.module.css';
+
 function CartList (props) {
+    const cartItems = [
+        { id: 'c1', name: 'sushi', amount: 2, price: 12.99 }, 
+    ].map((item) => <CartItem />)
+
     return (
         <Modal>
-            <div className={classes.cart}>
-                <CartItem />
-                <h2 className={classes.total}>Total Amount</h2>
-                <div>$100</div>
-                <div>
-                    <Button 
-                        onClick={props.onClick}
-                    >
-                        Close
-                    </Button>
-                    <Button>Order</Button>
-                </div>
+            <ul className={classes['cart-items']}>
+                {cartItems}
+            </ul>
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.action}>
+                <button className={classes['button-alt']}></button>
+                <button className={classes.button}></button>
             </div>
         </Modal>
     )
