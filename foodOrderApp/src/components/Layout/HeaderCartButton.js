ebd177a624f7847dd76132083ee57c6e841c2ced
Chocotapi <shioriavino@gmail.com>
2023-12-07 15:06:16 -0500

    setup number of cart items in Cart

--- foodOrderApp/src/components/Layout/HeaderCartButton.js
+++ foodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,17 @@
+import { useContext } from 'react';
+
+import CartContext from '../store/cart-context';
 import CartIcon from '../Cart/CartIcons';
 import classes from './HeaderCartButton.module.css';
 
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+        return currentNum + item.amount;
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}> 
             <span className={classes.icon}>
@@ -10,7 +19,7 @@ const HeaderCartButton = (props) => {
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
