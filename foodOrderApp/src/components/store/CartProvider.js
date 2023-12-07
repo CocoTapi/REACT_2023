8390d2f469548fd1b03a0471d3ca1ba3f4c951c4
Chocotapi <shioriavino@gmail.com>
2023-12-07 18:38:58 -0500

    added a condition to update item list

--- foodOrderApp/src/components/store/CartProvider.js
+++ foodOrderApp/src/components/store/CartProvider.js
@@ -8,8 +8,26 @@ const initialCartState = {
 
 const cartReducer = (state, action) => {
     if(action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.item.id
+        );
+        const exisingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+       
+        if(exisingCartItem) {
+            const updatedItem = {
+                ...exisingCartItem,
+                amount: exisingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem
+        } else {
+            updatedItems = state.items.concat(action.item);
+
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
