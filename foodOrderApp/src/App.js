ab9ecce8434c9aaf8b0ac61a8cdbc61d4d97d117
Chocotapi <shioriavino@gmail.com>
2023-12-09 00:07:29 -0500

    connected the fake database

--- foodOrderApp/src/App.js
+++ foodOrderApp/src/App.js
@@ -7,39 +7,6 @@ import CartList from './components/Cart/CartList';
 import CartProvider from './components/store/CartProvider';
 
 function App() {
-    const data = [
-      {
-          id: Math.floor(Math.random() * 10000),
-          name: 'Salmon Omusubi',
-          description: 'King salmon, white rice, seaweed',
-          price: 1.99
-      },
-      {
-          id: Math.floor(Math.random() * 10000),
-          name: 'Tuna mayo Omusubi',
-          description: 'Atlantic tuna mixed with Japanese mayonaise, white rice, seaweed',
-          price: 1.99
-      },
-      {
-          id: Math.floor(Math.random() * 10000),
-          name: 'Chicken Omusubi',
-          description: 'Teriyaki chicken, white rice, seaweed',
-          price: 1.99
-      },
-      {
-          id: Math.floor(Math.random() * 10000),
-          name: 'Ramen Egg Omusubi',
-          description: 'Ramen egg, white rice, seaweed',
-          price: 1.50
-      },
-      {
-          id: Math.floor(Math.random() * 10000),
-          name: 'Plum Omusubi',
-          description: 'Japanese pickled plum, white rice, seaweed',
-          price: 1.99
-      }
-  ];
-
   const [isCartOpen, setIsCartOpen] = useState(false);
 
     const openCartHandler = () => {
@@ -56,7 +23,7 @@ function App() {
       <Header onOpenCart={openCartHandler} />
       <main>
         <ShopTitle />
-        <MealList data={data} />
+        <MealList />
       </main>
     </CartProvider>
   );
--- foodOrderApp/src/components/Meals/MealList.js
+++ foodOrderApp/src/components/Meals/MealList.js
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import React from "react";
 
 import MealItem from "./MealItem";
 import Card from "../UI/Card";
 import classes from './MealList.module.css';
 
-const MealList = ({ data }) => {
+const MealList = () => {
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async() => {
+            const response = await fetch('https://react-http-d316b-default-rtdb.firebaseio.com/meals.json');
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price
+                });
+            }
+            setMeals(loadedMeals);
+        }
+        
+        fetchMeals();
+    }, []);
+
     let content;
-    content = data.map((item) => {
+    content = meals.map((item) => {
         return <MealItem key={item.id} item={item} />
         
     })
