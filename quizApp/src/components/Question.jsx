55cf738b732e7b34525872da21869d37bb8bd80a
Chocotapi <shioriavino@gmail.com>
2023-12-08 22:46:02 -0500

    updated timer function

--- quizApp/src/components/Question.jsx
+++ quizApp/src/components/Question.jsx
@@ -12,6 +12,16 @@ export default function Question ({
         selectedAnswer: '',
         isCorrect: null
     }); 
+
+    let timer = 10000;
+
+    if (answer.selectedAnswer) {
+        timer = 1000;
+    }
+
+    if (answer.isCorrect !== null) {
+        timer = 2000;
+    }
     
     function handleSelectAnswer(answer) {
         setAnswer({
@@ -33,7 +43,7 @@ export default function Question ({
 
     let answerState = '';
 
-    if (answer.selectedAnswe && answer.isCorrect !== null) {
+    if (answer.selectedAnswer && answer.isCorrect !== null) {
         answerState = answer.isCorrect ? 'correct' : 'wrong';
     } else if (answer.selectedAnswer) {
         answerState = 'answered';
@@ -42,8 +52,10 @@ export default function Question ({
     return (
         <div id="question">
             <QuestionTimer
-                timeout={10000} 
-                onTimeout={onSkipAnswer}
+                key={timer}
+                timeout={timer} 
+                onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
+                mode={answerState}
             />
             <h2>{QUESTIONS[index].text}</h2>
             <Answers 
