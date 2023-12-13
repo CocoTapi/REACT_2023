9aba60d1e935f5130411ac2777e122d8e71979a7
Chocotapi <shioriavino@gmail.com>
2023-12-13 14:35:11 -0500

    moved function using thunk

--- section_20/src/App.js
+++ section_20/src/App.js
@@ -4,8 +4,8 @@ import { Fragment, useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/slices/uiSlice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/slices/cartSlice';
 
 let isInitial = true;
 
@@ -17,44 +17,13 @@ function App() {
 
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data...'
-      }));
-      const response = await fetch('https://react-http-d316b-default-rtdb.firebaseio.com/cart.json', 
-        {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-        }
-      );
-
-      if(!response.ok) {
-        throw new Error('Sending cart data failed.')
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!'
-      }));  
-    };
-
     //avoid sending initial data
     if(isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!'
-      }))
-    });
     
+    dispatch(sendCartData(cart))
   }, [cart, dispatch]);
 
   return (
