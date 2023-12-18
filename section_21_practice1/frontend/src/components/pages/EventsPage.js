70305d5c42b5479509c2579dd613d4f1337f5cce
Chocotapi <shioriavino@gmail.com>
2023-12-18 15:55:54 -0500

    output the ID to every event detail page

--- section_21_practice1/frontend/src/components/pages/EventsPage.js
+++ section_21_practice1/frontend/src/components/pages/EventsPage.js
@@ -1,6 +1,24 @@
+import { Link } from 'react-router-dom';
+
+const dummy = [
+    { id: 'e1', title: 'ice skating', description: 'go to the ice skating!'},
+    { id: 'e2', title: 'Christmas market', description: 'More than 50 venders are selling delicious food, cute accessaries and greate christmas goods.'}
+];
+
 function EventsPage () {
     return (
-        <h1>Events List</h1>
+        <>
+            <h1>Events List</h1>
+            <ul>
+                {dummy.map((event) => {
+                    return (
+                        <li key={event.id}>
+                            <Link to={event.id} >{event.title}</Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
     )
 };
 
