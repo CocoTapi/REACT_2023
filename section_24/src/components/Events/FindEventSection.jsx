611254faa415e08416215d7e08e19734ced7ebc5
Chocotapi <shioriavino@gmail.com>
2023-12-23 22:22:20 -0500

    added search button function

--- section_24/src/components/Events/FindEventSection.jsx
+++ section_24/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,42 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import fetchEvents from '../../util/fetchEvents';
+import ErrorBlock from '../UI/ErrorBlock';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', {search: searchTerm}],
+    queryFn: () => fetchEvents(searchTerm)
+  })
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  }
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = <ErrorBlock title="Error is occured!" message={error.info?.message || 'Fail to find events.'} />
+  }
+
+  if (data) {
+    content = (
+      <ul className='events-list'>
+        {data.map((event) => 
+          <li key={event.id}><EventItem event={event}/></li>
+        )}
+      </ul>
+    )
   }
 
   return (
@@ -20,7 +52,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
