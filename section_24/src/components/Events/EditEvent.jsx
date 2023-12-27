f50cae74c6a5d5878e5f93e82741ae463bd347d4
Chocotapi <shioriavino@gmail.com>
2023-12-27 16:39:56 -0500

    added optimistic updating screen change

--- section_24/src/components/Events/EditEvent.jsx
+++ section_24/src/components/Events/EditEvent.jsx
@@ -3,10 +3,9 @@ import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { fetchEvent } from '../../util/http.js';
+import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
-import { updateEvent } from '../../util/http.js';
 
 export default function EditEvent() {
   const navigate = useNavigate();
@@ -18,7 +17,27 @@ export default function EditEvent() {
   })
 
   const { mutate } = useMutation({
-    mutationFn: updateEvent
+    mutationFn: updateEvent,
+    //once "mutate" is executed, this is called. not waited until mutation is done
+    onMutate: async (data) => {
+      const newEvent = data.event
+
+      //this not cancel mutation. just cancel query. this changes the detail page immediatery after pushing updating button
+      await queryClient.cancelQueries({ queryKey: ['events', params.id]});
+      const previousEvent =  queryClient.getQueryData(['events', params.id]);
+
+      queryClient.setQueriesData(['events', params.id], newEvent);
+      return { previousEvent }
+    },
+    //this avoid unacceptable updating
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent)
+    },
+    //no matter if this mutation succeed or not, it will be called. 
+    //even if the above optimistic updating became different from backend, this helps sync again
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id])
+    }
   })
 
   function handleSubmit(formData) {
