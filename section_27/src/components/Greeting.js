9b6ad2f3e1e2d268a9940260ab594062d72ca75a
Chocotapi <shioriavino@gmail.com>
2024-01-02 15:07:08 -0500

    added test function

---
+++ section_27/src/components/Greeting.js
@@ -0,0 +1,10 @@
+function Greeting(){
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <h2>It's goot to see you again!</h2>
+        </>
+    )
+}
+
+export default Greeting;
